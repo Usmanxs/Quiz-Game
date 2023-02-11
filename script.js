5dc444539e8dddb7ae05855564608c218766c9f4
Muhammad Usman <usmansheikh4949@gmail.com>
2023-02-11 23:46:59 +0500

    update

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-console.log("we are going to make the game that is interesting and easy to play")
-console.log("other things are more important")
+let user;
+if(user ===ture){
+  console.log("you win the game")
